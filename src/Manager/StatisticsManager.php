0502936ac88955082e4c397883368d6142cb1bb9
Alex Yatsenko <yatsenkolesh@gmail.com>
2022-08-16 14:37:11 +0100

    Stats interceptor async -> sync (#31)

--- src/Manager/StatisticsManager.php
+++ src/Manager/StatisticsManager.php
@@ -6,6 +6,7 @@ namespace Abrouter\Client\Manager;
 
 use Abrouter\Client\Events\EventDispatcher;
 use Abrouter\Client\DTO\EventDTOInterface;
+use Abrouter\Client\Events\Handlers\RelatedUsersStatisticsInterceptor;
 use Abrouter\Client\Services\ExperimentsParallelRun\ParallelRunSwitch;
 use Abrouter\Client\Services\Statistics\SendEventTask;
 
@@ -15,21 +16,34 @@ class StatisticsManager
 
     private ParallelRunSwitch $parallelRunSwitch;
 
+    private RelatedUsersStatisticsInterceptor $relatedUsersStatisticsInterceptor;
+
     /**
      * @param EventDispatcher $eventDispatcher
      * @param ParallelRunSwitch $parallelRunSwitch
+     * @param RelatedUsersStatisticsInterceptor $relatedUsersStatisticsInterceptor
      */
-    public function __construct(EventDispatcher $eventDispatcher, ParallelRunSwitch $parallelRunSwitch)
-    {
+    public function __construct(
+        EventDispatcher $eventDispatcher,
+        ParallelRunSwitch $parallelRunSwitch,
+        RelatedUsersStatisticsInterceptor $relatedUsersStatisticsInterceptor
+    ) {
         $this->eventDispatcher = $eventDispatcher;
         $this->parallelRunSwitch = $parallelRunSwitch;
+        $this->relatedUsersStatisticsInterceptor = $relatedUsersStatisticsInterceptor;
     }
 
     public function sendEvent(EventDTOInterface $eventDTO): void
     {
+        $task = new SendEventTask($eventDTO);
+        $isParallelRunningEnabled = $this->parallelRunSwitch->isEnabled();
+        if ($isParallelRunningEnabled) {
+            $this->relatedUsersStatisticsInterceptor->handle($task);
+        }
+
         $this->eventDispatcher->dispatch(
-            new SendEventTask($eventDTO),
-            $this->parallelRunSwitch->isEnabled()
+            $task,
+            $isParallelRunningEnabled
         );
     }
 }
